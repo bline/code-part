4003a1d16a516b267d8e8d8bebb646972c0f6154
Scott Beck <scottbeck@gmail.com>
2014-11-08 18:36:16 -0500

    for jshint on travis

--- bin/part.js
+++ bin/part.js
@@ -1,14 +1,15 @@
-'use strict';
-var part = require('./index.js');
+(function () {
+  'use strict';
+  var part = require('./index.js');
 
-var filePath = process.argv[2] || 't.js';
+  var filePath = process.argv[2] || 't.js';
 
-process.stdin.resume();
-var fileData = '';
-process.stdin.on('data', function (data) {
-  fileData += data.toString();
-});
-process.stdin.on('end', function () {
-  console.log(part(filePath, fileData));
-});
-
+  process.stdin.resume();
+  var fileData = '';
+  process.stdin.on('data', function (data) {
+    fileData += data.toString();
+  });
+  process.stdin.on('end', function () {
+    console.log(part(filePath, fileData));
+  });
+})();
