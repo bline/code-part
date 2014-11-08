cf89b9dcb5d64cc83ec1a94e0e0b986fe3fcc350
Scott Beck <scottbeck@gmail.com>
2014-11-08 15:34:54 -0500

    simple script to output json from stdin.

---
+++ bin/part.js
@@ -0,0 +1,14 @@
+
+var part = require('./index.js');
+
+var filePath = process.argv[2] || 't.js';
+
+process.stdin.resume();
+var fileData = '';
+process.stdin.on('data', function (data) {
+  fileData += data.toString();
+});
+process.stdin.on('end', function () {
+  console.log(part(filePath, fileData));
+});
+
